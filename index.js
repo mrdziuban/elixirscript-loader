cdb7a8facff6969b338cc3f2b1b8373a624ed833
Matt Dziuban <mrdziuban@gmail.com>
2017-02-21 21:29:54 -0500

    Update to compile entire context dir at once.

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ const exec = require('child_process').exec;
 const path = require('path');
 
 module.exports = function(source) {
-  var callback = this.async();
+  const callback = this.async();
   const tmp = path.join(__dirname, 'tmp');
   const mod = path.basename(this.resourcePath, '.exjs');
-  const cmd = `elixirscript '${source.replace(/\\/g, '\\\\').replace(/"/g, '\\"')}' --elixir -o '${tmp}'`;
+  const cmd = `elixirscript '${this.context}' -o '${tmp}'`;
 
   exec(cmd, function (error, stdout, stderr) {
-    if (error) {
-      return callback(error, null);
-    }
-    const out = `import ${mod} from '${path.join(tmp, 'app', `Elixir.${mod}.js`)}'; export default ${mod};`
-    return callback(null, out);
+    if (error) { return callback(error, null); }
+    callback(null, `import ${mod} from '${path.join(tmp, 'app', `Elixir.${mod}.js`)}'; export default ${mod};`);
   });
 };
