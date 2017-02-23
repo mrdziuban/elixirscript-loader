9595d6f825bdc01f5d175e9281819b7811ffe7b3
Matt Dziuban <mrdziuban@gmail.com>
2017-02-22 22:23:59 -0500

    Update for elixirscript master.

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ const path = require('path');
 module.exports = function(source) {
   const callback = this.async();
   const tmp = path.join(__dirname, 'tmp');
-  const mod = path.basename(this.resourcePath, '.exjs');
   const cmd = `elixirscript '${this.context}' -o '${tmp}'`;
   child_process.execSync(`rm -rf ${tmp}`);
 
   child_process.exec(cmd, function (error, stdout, stderr) {
     if (error) { return callback(error, null); }
-    const out = fs.readFileSync(path.join(tmp, 'app', `Elixir.${mod}.js`), 'utf8')
-      .replace(/from '\.\.\/elixir\//g, `from '${tmp}/elixir/`)
-      .replace(/from '\.\.\/app\//g, `from '${tmp}/app/`);
+    const out = fs.readFileSync(path.join(tmp, 'Elixir.App.js'), 'utf8')
+                  .replace(/import ([a-z]+) from '\.\//gi, `import $1 from '${tmp}/`);
     callback(null, out);
   });
 };
