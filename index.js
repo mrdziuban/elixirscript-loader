ed857cebf061b282a301045c97e9a0d26dea9916
Matt Dziuban <mrdziuban@gmail.com>
2017-02-23 09:46:11 -0500

    Replace incorrect module to fix bug (elixirscript/elixirscript#248).

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ module.exports = function(source) {
   child_process.exec(cmd, function (error, stdout, stderr) {
     if (error) { return callback(error, null); }
     const out = fs.readFileSync(path.join(tmp, 'Elixir.App.js'), 'utf8')
-                  .replace(/import ([a-z]+) from '\.\//gi, `import $1 from '${tmp}/`);
+                  .replace(/import ([a-z]+) from '\.\//gi, `import $1 from '${tmp}/`)
+                  .replace(/Elixir\.Enum\.member__qmark__/g, 'Bootstrap.Enum.member__qmark__');
     callback(null, out);
   });
 };
-
